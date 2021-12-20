e658d9b1ae9bfd7280ce48ef3dd69507e6793a5f
lestercch <lester_cch93@mesoamericana.edu.gt>
2021-12-20 16:35:11 -0600

    barcode different types

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!--Para que muestre los datos a nivel de html-->
+<!--El primer valor deberia ser "filepath" pero este no va-->
+<!--debido a que vamos a mostrar el codigo y no a guardarlo-->
+
+<!-- Parámetros:
+Text: «0123456789»
+Size: 40
+Code Type: Code39
+Orientation: Default = Horizontal
+Print: true -->
+
+<img src="barcode.php?text=0123456789&size=40&codetype=Code39&Orientation=vertical&sizefactor=1" />
